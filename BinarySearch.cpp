05b29762e3d94c00b21408d5b5a0fe01bdf8c950
Atasi Sharma <atasi2002@gmail.com>
2024-11-14 04:09:31 +0600

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-int binarysearch(int arr[], int n, int key)
+binarysearch(int t, int arr[], int key)
 {
-    int s=0, e=n-1, mid;
+    int s=0, e=t, mid;
     while(s<=e)
     {
         mid = (s+e)/2;
         if(arr[mid]>key)
-            e = mid - 1;
+            e = mid-1;
         else if(arr[mid]<key)
             s = mid+1;
         else
@@ -15,33 +15,37 @@ int binarysearch(int arr[], int n, int key)
     }
     return -1;
 }
+
 int main()
 {
-    int n, key, arr[100], y=1,x;
+    int t,y=1,key,x;
+    cout<<"Enter array size: ";
+    cin>>t;
+    int arr[t];
+    cout<<"Enter array elements:"<<endl;
+    for(int i=0; i<t; i++)
+    {
+        cin>>arr[i];
+    }
+
+    sort(arr, arr+t);
+    cout<<"Sorted array: "<<endl;
+    for(int i=0; i<t; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
     while(y==1)
     {
-        cout<<"Enter array size: "<<" ";
-        cin>>n;
-        cout<<"Enter the elements: "<<" ";
-        for(int i=0; i<n; i++)
-        {
-            cin>>arr[i];
-        }
-        sort(arr,arr+n);
-        cout<<"Sorted array: "<<" ";
-        for(int i=0; i<n; i++)
-        {
-            cout<<arr[i]<<endl;
-        }
-        cout<<"Enter the element you want to search: "<<endl;
+        cout<<"Enter the element you want to search: ";
         cin>>key;
-        x = binarysearch(arr,n,key);
-        if(x != -1)
-            cout<<"Element found at position "<<x+1<<endl;
+        x = binarysearch(t,arr,key);
+        if(x!= -1)
+            cout<<"Position: "<<x+1<<endl;
         else
             cout<<"Element not found"<<endl;
 
         cout<<"Do you want to continue?"<<endl<<"If 'yes' press 1, else press 0"<<endl;
         cin>>y;
     }
+    return 0;
 }
